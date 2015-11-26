93e7cf87289db178950f4f95805644fe64c853e3
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2015-11-26 01:05:03 +0100

    first commit

---
+++ public/src/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by med on 26/11/15.
+ */
+
+angular.module("ContactApp", [])
+    .run(function ($rootScope) {
+        $rootScope.message = "Hello Angular !";
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by med on 25/11/15.
+ */
+
+var express = require('express'),
+    app     = express();
+
+app
+    .use(express.static('./public'))
+    .get('*', function(req, res) {
+       res.sendfile('public/main.html');
+    })
+    .listen(3000);
