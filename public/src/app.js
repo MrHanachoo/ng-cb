e8a3266b507ebc94176878d5b7b5d775402ff044
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2015-11-26 01:28:50 +0100

    first route/controller

--- public/src/app.js
+++ public/src/app.js
@@ -2,7 +2,13 @@
  * Created by med on 26/11/15.
  */
 
-angular.module("ContactApp", [])
-    .run(function ($rootScope) {
-        $rootScope.message = "Hello Angular !";
+angular.module("ContactApp", ["ngRoute"])
+    .config(function ($routeProvider, $locationProvider) {
+       $routeProvider
+           .when("/contacts", {
+                   controller: 'ListController',
+                   templateUrl: 'views/list.html'
+           });
+            $locationProvider.html5Mode(true);
+
 });
---
+++ public/src/controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by med on 26/11/15.
+ */
+
+angular.module("ContactApp")
+    .controller("ListController", function ($scope) {
+       $scope.contacts = [];
+    });
