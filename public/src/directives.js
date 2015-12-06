557b5093c1b448197bfe69894c41aab1375efff8
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2015-12-06 15:21:50 +0100

    control field

--- public/src/directives.js
+++ public/src/directives.js
@@ -16,3 +16,38 @@ angular.module("ContactsApp")
         tel: ['Phone Number', 'should be a phone number'],
         color: ['Color', 'should be a color']
     })
+    .directive('formField', function($timeout, FieldTypes) {
+            return {
+                restrict: 'EA',
+                templateUrl: 'views/form-field.html',
+                replace: true,
+                scope: {
+                    record: '=',
+                    field: '@',
+                    live: '@',
+                    required: '@'
+                },
+                link: function ($scope, element, attr) {
+                    $scope.types = FieldTypes;
+                    $scope.remove = function (field) {
+                        delete $scope.record[field];
+                        $scope.blurUpdate();
+                    };
+
+                    $scope.blurUpdate = function() {
+                      if ($scope.live !== 'false') {
+                          $scope.record.$update(function (updateRecord) {
+                              $scope.record = updateRecord;
+                          });
+                      }
+                    };
+                    var saveTimeout;
+                    $scope.update = function () {
+                        $timeout.cancel(saveTimeout);
+                        saveTimeout = $timeout($scope, blurUpdate, 1000);
+                    };
+
+
+                }
+            };
+    });
